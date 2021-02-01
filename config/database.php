0189862c8b12987e7b1b28ada0d533d33dfa0201
Otto Reimers <70694917+ottoreimers@users.noreply.github.com>
2021-02-01 12:01:25 +0100

    changed config

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,8 +69,8 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST'),
             'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
