75b619a7aca85353cfba9456de3ec0c6bf564371
Otto Reimers <70694917+ottoreimers@users.noreply.github.com>
2021-02-01 12:57:46 +0100

    changes app file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->envrionment('production')) {
+        if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
     }
